f7a9447431e0ae25427d439ab631ee020244849d
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-13 21:47:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 function fibonacci(num) {
-    if (num === 0) {
-        return 0;
-    } else if (num === 1) {
-        return 0;
-    } else if (num === 2) {
-        return 1;
-    }
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 
     let a = 0, b = 1;
-    for (let i = 2; i < num; i++) {
-        [a, b] = [b, a + b];
+    for (let i = 3; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
     }
-
     return b;
 }
 
-// Test cases
-console.log(fibonacci(1)); // Output: 0
-console.log(fibonacci(5)); // Output: 3
+
